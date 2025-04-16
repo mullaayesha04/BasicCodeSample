51e6d11f257ad3416ae2297a669e81855269f5b3
Ayesha <82706008+mullaayesha04@users.noreply.github.com>
2025-04-16 15:13:37 +0530

    Create PalindromeExample.cs

---
+++ PalindromeExample.cs
@@ -0,0 +1,19 @@
+class StrPalindrome
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Enter Input String");
+    string inputStr=Console.ReadLine();
+
+    string reversedStr=new string(inputStr.Reverse().ToArray());
+
+    if(inputStr==reversedStr)
+    {
+      
+    }
+    else
+    {
+      
+    }
+  }
+}
